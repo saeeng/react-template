162182932324a1935aa038d5e735aa13c7c71d5b
saeeng <saeeng.opt@gmail.com>
2024-02-19 10:43:42 +0900

    Initial commit

---
+++ .babelrc.cjs
@@ -0,0 +1,28 @@
+const plugins = ['@babel/plugin-proposal-class-properties'];
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+if (!isProduction) {
+  plugins.push('react-refresh/babel');
+}
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        debug: isProduction,
+        useBuiltIns: 'usage',
+        corejs: 3.36
+      }
+    ],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic'
+      }
+    ],
+    '@babel/preset-typescript'
+  ],
+  plugins
+};
